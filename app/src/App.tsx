7dd1b36f594afe0ae55d9babf8e6cf02a9ebc267
Brandon Liu <bdon@bdon.org>
2024-11-15 18:59:34 +0800

    type fixes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,28 +1,27 @@
-import { createSignal, createEffect, onMount } from "solid-js";
+import { onMount } from "solid-js";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./App.css";
 import maplibregl from "maplibre-gl";
+import { LayerSpecification } from "maplibre-gl";
 import { layersWithCustomTheme } from "protomaps-themes-base";
 
 const THEMES = ["contrast"];
 
 function App() {
-  let map: maplibregl.Map;
-
   onMount(async () => {
     maplibregl.setRTLTextPlugin(
       "https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js",
       true,
     );
 
-    let layers;
+    let layers: LayerSpecification[] = [];
 
     for (let themeName of THEMES) {
       let theme = await import(`../../themes/${themeName}.ts`);
       layers = layersWithCustomTheme("protomaps", theme.default, "en");
     }
 
-    map = new maplibregl.Map({
+    new maplibregl.Map({
       container: "map",
       style: {
         version: 8,
